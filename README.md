d591c38ea6b7b7a9461fad5d79c895e9e5783daa
Case Gibson <t.barbato78@gmail.com>
2023-07-11 11:46:57 +0200

    feat: pre-commit try

--- README.md
+++ README.md
@@ -73,5 +73,5 @@ Dans le reste de la documentation sur le développement local, il est supposé q
 
 Utilisation de PowerShell, comme ci-dessus sauf :
 
-- Pour activer l'environnement virtuel, `.\venv\Scripts\Activate.ps1` 
+- Pour activer l'environnement virtuel, `.\venv\Scripts\Activate.ps1`
 - Remplacer `which <my-command>` par `(Get-Command <my-command>).Path`
--- lettings/models.py
+++ lettings/models.py
@@ -8,7 +8,9 @@ class Address(models.Model):
     city = models.CharField(max_length=64)
     state = models.CharField(max_length=2, validators=[MinLengthValidator(2)])
     zip_code = models.PositiveIntegerField(validators=[MaxValueValidator(99999)])
-    country_iso_code = models.CharField(max_length=3, validators=[MinLengthValidator(3)])
+    country_iso_code = models.CharField(
+        max_length=3, validators=[MinLengthValidator(3)]
+    )
 
     def __str__(self):
         return f"{self.number} {self.street}"
