e3af5f021fadccc7d738193cedcf6de25626b787
Case Gibson <t.barbato78@gmail.com>
2023-07-22 21:52:32 +0200

    feat: sentry begin

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,9 +1,6 @@
 import os
 import sentry_sdk
-from django.core.management.utils import get_random_secret_key
 from sentry_sdk.integrations.django import DjangoIntegration
-from sentry_sdk.integrations.wsgi import SentryWsgiMiddleware
-import wsgi
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -14,7 +11,9 @@ OCL_DIR = BASE_DIR + "/oc_lettings_site"
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_random_secret_key()
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY", default="fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -128,7 +127,6 @@ sentry_sdk.init(
     send_default_pii=True,
 )
 
-wsgi_app = SentryWsgiMiddleware(wsgi)
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
