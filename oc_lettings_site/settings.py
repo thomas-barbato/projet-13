0a21359a3fad1b55877f8c2fb225bc62c8adf19c
Case Gibson <t.barbato78@gmail.com>
2023-07-22 20:46:07 +0200

    feat: sentry

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,4 +1,6 @@
 import os
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -100,6 +102,29 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+# Sentry configuration
+sentry_sdk.init(
+    dsn=(
+        "https://243e2108953d4e61a9948cd026257615@o4505574491815936"
+        ".ingest.sentry.io/4505574494896128"
+    ),
+    integrations=[
+        DjangoIntegration(
+            transaction_style="url",
+            middleware_spans=True,
+            signals_spans=False,
+            cache_spans=False,
+        )
+    ],
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True,
+)
 
 
 # Internationalization
--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -3,9 +3,16 @@ from django.urls import path, include
 
 from . import views
 
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
+    return division_by_zero
+
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("", include("profiles.urls")),
     path("", include("lettings.urls")),
+    path("sentry-debug/", trigger_error),
     path("admin/", admin.site.urls),
 ]
