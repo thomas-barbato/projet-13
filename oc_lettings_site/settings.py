29c9526777d7967a29512577ca29878c7b6682fd
Case Gibson <t.barbato78@gmail.com>
2023-08-02 09:44:16 +0200

    feat: .env

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -11,7 +11,6 @@ print(BASE_DIR)
 
 env = environ.Env()
 environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
-print(env)
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("DJANGO_SECRET_KEY", default=get_random_secret_key())
