66ab21278a2118bf0e4e39ff5f911e8b8d53ff7a
Case Gibson <t.barbato78@gmail.com>
2023-08-02 08:58:26 +0200

    feat: .env

--- setup_env.py
+++ setup_env.py
@@ -1,5 +1,11 @@
+from django.core.management.utils import get_random_secret_key
+
 with open(".env", "w") as f:
-    f.write("SENTRY_DSN= \n")
+    f.write(f"DJANGO_SECRET_KEY={get_random_secret_key()}\n")
+    f.write(
+        "SENTRY_DSN=https://8818e1035f7441f4bb83c122d9388456@o4505601723269120.ingest.sentry.io"
+        "/4505624010227712"
+    )
     f.close()
 
 print(".env creation done")
